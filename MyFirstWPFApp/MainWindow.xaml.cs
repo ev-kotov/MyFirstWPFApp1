5ff6b4d4fd0bbb9001a5a9f474050e4f8ef7f139
pykot <superkotopess@gmail.com>
2022-02-19 00:31:58 +0300

    add two methods operatiopns with money

--- MyFirstWPFApp/MainWindow.xaml.cs
+++ MyFirstWPFApp/MainWindow.xaml.cs
@@ -1,46 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace MyFirstWPFApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        private double _bankAccount = 0; // банковский счёт
 
-        private void AddMoneyToAccount(object sender, RoutedEventArgs e)
+        // принимаем средсва
+        private void InitialMoney(object sender, RoutedEventArgs e)
         {
             try
             {
-                // принимаем средсва
-                double bankAccount = Convert.ToDouble(bankAccountTextBox.Text);
+                double inputData = Convert.ToDouble(bankAccountTextBox.Text);
 
-                if (bankAccount <= 0)
+                if (inputData <= 0)
                 {
-                    MessageBox.Show("Вносимая сумма не может быть меньшей или равной нулю");
+                    MessageBox.Show("Вносимая сумма не может быть отрицательной или равной нулю");
                 }
                 else
                 {
+                    _bankAccount += inputData;
+
                     MessageBox.Show(
                         $" Операция проведена успешно." +
-                        $"\n На счету: {bankAccount.ToString()}");
+                        $"\n Внесено: {inputData}" +
+                        $"\n На счету: {_bankAccount.ToString()}");
+                    
                 }
             }
             catch (Exception)
@@ -48,34 +38,46 @@ namespace MyFirstWPFApp
                 MessageBox.Show("Введены недопустимые символы, повторите ввод");
             }
         }
+
+        // снимаем средства
+        private void WithdrawMoney(object sender, RoutedEventArgs e) 
+        {
+            try
+            {
+                double inputData = Convert.ToDouble(bankAccountTextBox.Text);
+
+                if (inputData <= 0)
+                {
+                    MessageBox.Show("Вносимая сумма не может быть отрицательной или равной нулю");
+                }
+                else
+                {
+                    if (_bankAccount >= inputData)
+                    {
+                        _bankAccount -= inputData;
+
+                        MessageBox.Show(
+                        $" Операция проведена успешно." +
+                        $"\n Снято: {inputData}" +
+                        $"\n На счету: {_bankAccount.ToString()}");
+                    }
+                    else
+                    {
+                        MessageBox.Show(
+                            $" На счету недостаточно средств" +
+                            $"\n На счету: {_bankAccount.ToString()}");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введены недопустимые символы, повторите ввод");
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
     }
-    //class Account
-    //{
-    //    public delegate void AccountHandler(string message);
-    //    public event AccountHandler? Notify;
-
-    //    // 1.Определение события
-    //    public Account(int sum) => Sum = sum;
-    //    public int Sum { get; private set; }
-
-        
-    //    public void Put(int sum) // Вызов события
-    //    {
-    //        Sum += sum;
-    //        Notify?.Invoke($"На счет поступило: {sum}");
-    //    }
-
-    //    public void Take(int sum) // Вызов события
-    //    {
-    //        if (Sum >= sum)
-    //        {
-    //            Sum -= sum;
-    //            Notify?.Invoke($"Со счета снято: {sum}");
-    //        }
-    //        else
-    //        {
-    //            Notify?.Invoke($"Недостаточно денег на счете. Текущий баланс: {Sum}"); ;
-    //        }
-    //    }
-    //}
 }
